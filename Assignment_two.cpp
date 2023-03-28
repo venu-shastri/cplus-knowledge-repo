3fb087ee771ceddd6b4611dd60d8419d7cccf15c
Venugopal K <venu.shastri@gmail.com>
2023-03-28 17:11:34 +0530

    Create Assignment_two.cpp

---
+++ Assignment_two.cpp
@@ -0,0 +1,23 @@
+class Door {
+public:
+	void open(){}
+	void close()
+};
+
+class Buzzer{
+
+public:
+	void makeNoise(){}
+};
+
+class  SMSManager{
+
+	public :void sendNotificationMessage() {
+
+	 }
+};
+
+class VisualizationManager {
+	public:void turnVideoOn() {}
+	public:void turnVideoOff() {}
+};
