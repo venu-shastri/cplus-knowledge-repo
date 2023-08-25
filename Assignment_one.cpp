daa2af46199a4399dbec250e23f05dbf42902e4d
Venugopal K <venu.shastri@gmail.com>
2023-08-25 12:20:10 +0530

    Update Assignment_one.cpp

--- Assignment_one.cpp
+++ Assignment_one.cpp
@@ -1,49 +1,56 @@
+class ICommunicationManager{
+public:
+  virtual void shareContent(string imageLink)=0;
+}
 class ImageGallery {
-	string images[5] = { "one.jpg","two.jpg","three.jpg","four.jpg","fiv.jpg"};
-public:void share(int index) {
 
-
-
+string images[5] = { "one.jpg","two.jpg","three.jpg","four.jpg","fiv.jpg"};
+public:void share(int index,ICommunicationManager* comManager) {
+	comManager->shareContent(images[index]);
 }
 };
 
-class BluetoothManager{
+class BluetoothManager: public ICommunicationManager {
 public:
 	void searchDevices() {}
     void pairdevice(){}
 	void sendContent() {}
 	void turnOn() {}
 	void turnOff(){}
+void shareContent(string imageLink){   // }
 
 };
 
-class WhatsUpManager {
+class WhatsUpManager : public ICommunicationManager {
 	void turnOn() {}
 	void turnOff() {}
 	void shareContent() {}
 	void joinGroup() {}
 	void addMember(){}
 	void createGroup() {}
-
+	void shareContent(string imageLink){   // }
 
 };
 
-class MailManager {
+class MailManager:public ICommunicationManager {
 
 	void pullNewMails() {}
 	void senMails(){}
 	void composeEmail(){}
 	void addAttachement() {}
+void shareContent(string imageLink){   // }
 
 };
 
 int main() {
 
 	MailManager* _mailManager = new  MailManager();
-	BluetoothManager* _mailManager = new  BluetoothManager();
-	WhatsUpManager* _mailManager = new  WhatsUpManager();
+	BluetoothManager* _bluetoothManager = new  BluetoothManager();
+	WhatsUpManager* _whatsUpManager = new  WhatsUpManager();
 	
 	ImageGallery* imageMgmt = new  ImageGallery();
-	imageMgmt->share(0);
+	imageMgmt->share(0,_mailManager);
+	imageMgmt->share(0,_bluetoothManager);
+	imageMgmt->share(0,_whatsUpManager);
 	
 }
